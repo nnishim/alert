f4fccfced50efc66908d295ab9a22a2aec4925e2
nnishim <nursishimov@gmail.com>
2022-01-28 16:13:59 +0600

    почти закончил

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 const overlayContainer = document.querySelector('.overlay-container');
-const overlayContent = overlayContainer.querySelector('.content');
+const overlay = document.querySelector('.overlay');
 const html = `
         <h2 class="title">Alert!</h2>
         <div class="desc">
           Обычный текст с <b> html </b> элементами
         </div>
-        <button class="okay" onclick="deact()">OK</button>
-`
+        <button class="okay" onclick="deact()"> OK <i class="bi bi-check btn-icon"></i></button>
+`;
 
 
-overlayContainer.onclick = function (event){
-  deactivate(event)
+overlayContainer.onclick = function (){
+  deactivate(event);
 }
 
 function activate(){
-  overlayContent.innerHTML = html
-  overlayContainer.style.display = `flex`
-  document.body.classList.add('no-scroll')
+  overlay.innerHTML = html;
+  overlayContainer.style.display = `flex`;
+  overlay.style.display = `block`;
+  document.body.classList.add('no-scroll');
 }
 function deactivate(event){
   if(event.target !== overlayContainer){
     return
   }
-  deact()
+  deact();
 }
 function deact(){
-  overlayContainer.style.display = `none`
-  document.body.classList.remove('no-scroll')
+  overlayContainer.style.display = `none`;
+  overlay.style.display = `none`;
+  document.body.classList.remove('no-scroll');
 }
